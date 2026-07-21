c015ce103762f2973e270cbb0937f3448255db6c
root <root@localhost>
2026-07-21 17:37:27 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Example Activity Project
+
+This project serves as an example of using Activity in Android development. It demonstrates how to integrate Activity components into your Android app.
+
+## Libraries Used
+
+- **None**
---
+++ app/src/main/java/com/cool/app/ons/Main.java
@@ -0,0 +1,14 @@
+package com.cool.app.ons;
+
+import android.app.*;
+import android.os.*;
+
+public class Main extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
